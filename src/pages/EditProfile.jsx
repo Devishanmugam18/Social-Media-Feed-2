1fa37a310efa6bc22f21f91ebc5fcd6fe3a8d859
Devi Shanmugam <you@example.com>
2025-11-21 19:29:59 +0530

    save profile edit form

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -4,9 +4,36 @@ import profilePic from "../assets/FeedPage/ProfilePic.jpg";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import EditIcon from "@mui/icons-material/Edit";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function EditProfile() {
   const navigate = useNavigate();
+  const [userInfo, setuserInfo] = useState({ Uname: "", bio: "" });
+
+  useEffect(() => {
+    const saved = localStorage.getItem("userInfo");
+    setuserInfo(
+      saved
+        ? JSON.parse(saved)
+        : {
+            Uname: "Sakshi Agarwal",
+            bio: "Just someone who loves designing, sketching, and finding beauty in the little things 💕",
+          }
+    );
+  }, []);
+
+  // useEffect(() => {
+
+  // }, [userInfo]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    console.log("submitted");
+
+    console.log(userInfo);
+  };
+
   return (
     <Box>
       <Box sx={{ marginBottom: "-1.5em", position: "relative" }}>
@@ -47,7 +74,7 @@ function EditProfile() {
             width: "30px",
             height: "30px",
             bgcolor: "#F4F4F4",
-            border: "F4F4F4",
+            border: "#F4F4F4",
           }}
         >
           <EditIcon sx={{ fontSize: "1rem", color: "#000000" }} />
@@ -77,38 +104,49 @@ function EditProfile() {
             <EditIcon sx={{ fontSize: "1rem", color: "#000000" }} />
           </Avatar>
         </Box>
-        <Box sx={{ m: "0 1.2em" }}>
-          <TextField
-            variant="standard"
-            label="Name"
-            defaultValue={"Sakshi Agarwal"}
-            fullWidth
-          />
-          <TextField
-            variant="standard"
-            label="Bio"
-            defaultValue={
-              "Just someone who loves designing, sketching, and finding beauty in the little things 💕"
-            }
-            fullWidth
-            sx={{ mt: "1em" }}
-            multiline
-          />
-        </Box>
-        <Button
-          variant="contained"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            margin: "9em auto",
-            width: "90%",
-            borderRadius: "5em",
-            bgcolor: "black",
-          }}
-        >
-          Save
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ m: "0 1.2em" }}>
+            <TextField
+              value={userInfo.Uname}
+              onChange={(e) =>
+                setuserInfo({ ...userInfo, Uname: e.target.value })
+              }
+              variant="standard"
+              label="Name"
+              // defaultValue={"Sakshi Agarwal"}
+              fullWidth
+            />
+            <TextField
+              value={userInfo.bio}
+              onChange={(e) =>
+                setuserInfo({ ...userInfo, bio: e.target.value })
+              }
+              variant="standard"
+              label="Bio"
+              // defaultValue={
+              //   "Just someone who loves designing, sketching, and finding beauty in the little things 💕"
+              // }
+              fullWidth
+              sx={{ mt: "1em" }}
+              multiline
+            />
+          </Box>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              margin: "9em auto",
+              width: "90%",
+              borderRadius: "5em",
+              bgcolor: "black",
+            }}
+          >
+            Save
+          </Button>
+        </form>
       </Box>
     </Box>
   );
